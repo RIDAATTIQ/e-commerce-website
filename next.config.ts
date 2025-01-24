6d604f367828ea649f16abdf83535e7164844631
rida shiwani <167556213+RIDAATTIQ@users.noreply.github.com>
2025-01-24 13:55:54 -0800

    Updated next.config.ts to configure Sanity image domain

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
   async rewrites() {
     return [
       { source: '/studio', destination: '/studio/index.html' },
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,5 @@
 import { client } from '@/sanity/lib/client';
+
 import Image from 'next/image';
 
 type product ={
